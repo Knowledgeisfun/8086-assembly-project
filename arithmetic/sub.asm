d73d9fa7978c6df58020fd21f6cfed7dda2f869e
Knowledgeisfun <142772826+Knowledgeisfun@users.noreply.github.com>
2025-05-31 12:18:25 +0530

    Update sub.asm

--- arithmetic/sub.asm
+++ arithmetic/sub.asm
@@ -1,1 +1,34 @@
 
+DATA SEGMENT
+    A       DB 0A7H        ; Define byte A with value 0xA7 (decimal 167)
+    B       DB 9AH         ; Define byte B with value 0x9A (decimal 154)
+    RESULT  DB ?           ; Reserve a byte for the subtraction result
+    BORROW  DB ?           ; Reserve a byte to store borrow flag (0 or 1)
+DATA ENDS
+
+CODE SEGMENT
+    ASSUME CS:CODE DS:DATA
+    
+START:
+    MOV AX, DATA           ; Load address of DATA segment into AX
+    MOV DS, AX             ; Initialize DS register to point to DATA segment
+    
+    MOV AL, A              ; Load value of A into AL register (lower byte of AX)
+    SUB AL, B              ; Subtract B from AL; sets flags including Carry (borrow)
+    
+    MOV RESULT, AL         ; Store the subtraction result in RESULT
+    
+    JC BORROW              ; Jump to BORROW label if Carry Flag (borrow) is set
+    
+    MOV BORROW, 00H        ; If no borrow, store 0 in BORROW
+    JMP CONTINUE           ; Jump to CONTINUE to skip borrow handling
+    
+BORROW:
+    MOV BORROW, 01H        ; If borrow occurred, store 1 in BORROW
+    
+CONTINUE:
+    MOV AH, 4CH            ; DOS function code for program termination
+    INT 21H                ; Interrupt to terminate program and return control to OS
+    
+CODE ENDS
+END START
